31bde1efc9060f17e0f2bcb0420e5d2fb02f7e6f
Nouman A Khan <91853465+diginak@users.noreply.github.com>
2021-10-07 20:59:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Naureen !!")
+    basic.showString("")
     while (true) {
         pins.analogSetPitchPin(AnalogPin.P0)
-        music.playMelody("E B C5 A B G A F ", 263)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    while (true) {
-        basic.showString("Nouman !!")
-        pins.analogSetPitchPin(AnalogPin.P0)
-        music.playMelody("A A A A A A A A ", 185)
+        music.setVolume(255)
+        led.plot(4, 1)
+        music.playMelody("E B C5 A B G A F ", 40)
     }
 })
